9c07e192aa551d870c5815c6965f9f1a435eb07a
Zaenal <ahmadzaenal125@gmail.com>
2024-11-23 13:20:05 +0700

    delete unused files

--- install_in_new_server.sh
+++
@@ -1,8 +0,0 @@
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.12 python3.12-venv
-python3.12 -m venv .dev
-
-. .dev/bin/activate
-
-pip install -r requirements.txt
